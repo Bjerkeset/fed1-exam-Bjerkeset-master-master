1c2f378d2fdb3ff8d00779794e2be6221f3fb302
Bjerkeset <bb.bjerk@gmail.com>
2023-05-11 08:51:36 +0200

    vite config update

--- fed1-exam-Bjerkeset/vite.config.js
+++ fed1-exam-Bjerkeset/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  root: "./fed1-exam-Bjerkeset-master-master/fed1-exam-Bjerkeset",
   server: {
     host: "localhost",
     port: 3000,
@@ -10,11 +9,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: path.resolve("index.html"),
-        contact: path.resolve("pages/contact.html"),
-        about: path.resolve("pages/about.html"),
-        blog: path.resolve("pages/blogs.html"),
-        blogDetail: path.resolve("pages/blog-detail.html"),
+        main: path.resolve(__dirname, "index.html"),
+        contact: path.resolve(__dirname, "pages/contact.html"),
+        about: path.resolve(__dirname, "pages/about.html"),
+        blog: path.resolve(__dirname, "pages/blogs.html"),
+        blogDetail: path.resolve(__dirname, "pages/blog-detail.html"),
       },
     },
   },
